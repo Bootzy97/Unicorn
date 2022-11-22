63364e56a0083eaf51ff3e13ff48f6b0d17af72b
Michael Butzlaff <98661433+Bootzy97@users.noreply.github.com>
2022-11-22 11:01:06 -0600

    Fix to config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId:         'us-east-2_ou57LbDQz',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '7g1oe9v55jdd8css4j97ng630u',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId:         'us-east-2_gOAdMlq93',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '7pv91i1gudcmaa6j2sv2focoqf',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
